289ae7de95023126a8284e689b54ed9d5d211b23
Reemaalazri <146677257+Reemaalazri@users.noreply.github.com>
2026-05-07 11:39:56 +0100

    Add crawler URL normalisation tests

---
+++ tests/test_crawler.py
@@ -0,0 +1,25 @@
+"""
+Tests for the crawler module.
+"""
+
+from src.crawler import WebCrawler
+
+
+def test_normalise_url_removes_fragment() -> None:
+    crawler = WebCrawler("https://quotes.toscrape.com/", politeness_delay=0)
+
+    result = crawler._normalise_url(
+        "https://quotes.toscrape.com/page/1/#comments"
+    )
+
+    assert result == "https://quotes.toscrape.com/page/1"
+
+
+def test_normalise_url_removes_trailing_slash_except_root() -> None:
+    crawler = WebCrawler("https://quotes.toscrape.com/", politeness_delay=0)
+
+    page_url = crawler._normalise_url("https://quotes.toscrape.com/page/1/")
+    root_url = crawler._normalise_url("https://quotes.toscrape.com/")
+
+    assert page_url == "https://quotes.toscrape.com/page/1"
+    assert root_url == "https://quotes.toscrape.com/"
